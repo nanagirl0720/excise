63853b237687a8cb1f57ff890e2015b7b2c86404
pxn <pu6422534@163.com>
2019-09-18 11:01:16 +0800

    测试yml和组件的映射

--- src/test/java/com/day/excise/ExciseApplicationTests.java
+++ src/test/java/com/day/excise/ExciseApplicationTests.java
@@ -1,7 +1,9 @@
 package com.day.excise;
 
+import com.day.excise.bean.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ExciseApplicationTests {
 
+    @Autowired
+    User user;
     @Test
     public void contextLoads() {
+        System.out.print(user);
     }
 
 }
